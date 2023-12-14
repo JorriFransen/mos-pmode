77296fa604aa42c2e6f25b8410e186128acfa3ff
Jorri Fransen <jorri@jorrifransen.nl>
2023-12-14 22:06:58 +0100

    Switching back to 16-bit read from 32-bit protected.

--- src/main.asm
+++ src/main.asm
@@ -78,6 +78,72 @@ start:
     jmp .loop
 
 .done:
+    ; go back to real mode
+    ; 1. Disable interrupts. A CLI instruction disables maskable hardware interrupts. NMI interrupts can be disabled with external circuitry.
+    cli
+    ; Not using NMI
+
+    ; 2. If paging is enabled, perform the following operations:
+    ;       - Transfer program control to linear addresses that are identity mapped to physical addresses (that is, linear addresses equal physical addresses).
+    ;       - Ensure that the GDT and IDT are in identity mapped pages.
+    ;       - Clear the PG bit in the CR0 register.
+    ;       - Mov 0H into the CR3 register to flush the TLB.
+
+    ; Not using paging
+
+    ; 3. Transfer program control to a readable segment that has a limit of 64 KBytes (FFFFH). This operation loads the CS register with the segment limit equired in real-address mode.
+    jmp word 0x18:.pmode16
+
+.pmode16:
+    [bits 16]
+
+    ; 4. Load segment registers SS, DS, ES, FS, and GS with a selector for a descriptor containing the following values, which are appropriate for real-address mode:
+    ;       - Limit = 64 KBytes (0FFFFH)
+    ;       - Byte granular (G = 0)
+    ;       - Expand up (E = 0)
+    ;       - Writable (W = 1)
+    ;       - Present (P = 1)
+    ;       - Base = any value
+    ;   The segment registers must be loaded with non-null segment selectors or the segment registers will be unusable in real-address mode. Note that if the segment registers are not reloaded, execution continues using the descriptor attributes loaded during protected mode..
+
+    ; Not accessing memory yet, so skip no.4 for now..
+
+    ; 5. Execute an LIDT instruction to point to a real-address mode interrupt table that is withint the 1MByte real-address mode address range.
+
+    ; We haven't changed the IDT (yet)
+
+    ; 6. Clear the PE flag in the CR0 register to switch to real-address mode.
+    mov eax, cr0
+    and eax, ~1
+    mov cr0, eax
+
+    ; 7. Execute a far JMP instruction to jump to a real-address mode program. This operation flushes the instruction queue and loads the appropriate base-address value in the CS register
+    jmp word 00h:.rmode
+
+.rmode:
+    ; 8. Load the SS, DS, ES, FS, and GS registers as needed by the real-address mode code. If any of the registers are not going to be used in real-address mode, write 0s to them.
+    mov ax, 0
+    mov ds, ax
+    mov ss, ax
+
+    ; 9. Execute the STI instruciton to enable maskable hardware interrupts and perform the necessary hardware operation to enable NMI interrupts.
+    sti
+    ; Not using NMI
+
+
+    ; print hello world using int 10h
+    mov si, g_Hello_16
+
+.rloop:
+    lodsb
+    or al, al
+    jz .rdone
+    mov ah, 0eh
+    int 10h
+    jmp .rloop
+
+.rdone:
+
 
 .halt:
     jmp .halt
@@ -185,7 +251,8 @@ g_GDT:
 g_GDT_Decriptor:    dw g_GDT_Decriptor - g_GDT - 1 ; size of gdt
                     dd g_GDT                       ; offset (address of gdt)
 
-g_Hello:            db "Hello world!", 0
+g_Hello:            db "Hello world from protected mode!", 0
+g_Hello_16:         db "Hello world from real mode!", 0
 
 times 510-($-$$) db 0
 dw 0xAA55
